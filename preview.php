4ac6e05d40ed0992a4b2768cc3f93e33e0077732
SIMONE FIORAVANTI <simospa@gieffeedizioni.it>
2022-06-14 13:52:08 +0200

    Load CP in preview (#1)

--- preview.php
+++ preview.php
@@ -1,38 +1,34 @@
 <?php
 
+// Load CP.
+require_once('../../../wp-load.php');
+
 /*
-This file is called directly so I don't have CP escaping function.
+Here we don't check for nonces because this link is intended
+to be accessed directly.
 */
 
-// Check that everything is in place.
-if (!isset($_REQUEST['preview']) || !isset($_REQUEST['redirect']) || !isset($_SERVER['REQUEST_SCHEME']) || !isset($_SERVER['SERVER_NAME'])) { //phpcs:ignore WordPress.Security.NonceVerification.Recommended
+// Delete the cookie if there is no key.
+if (!isset($_REQUEST['preview'])) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 	if (isset($_COOKIE['selective_preview'])) {
 		setcookie('selective_preview', '', time() - 3600);
 	}
 	exit;
 }
 
-// Sanitize the value og the cookie.
-$value = preg_replace('/[^0-9]/', '', $_REQUEST['preview']); //phpcs:ignore
+// Sanitize the value of the cookie.
+$key = preg_replace('/[^0-9]/', '', sanitize_key(wp_unslash($_REQUEST['preview']))); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 
-// Get where to redirect after the cookie is set and validate it.
-if (!isset($_REQUEST['redirect'])) { //phpcs:ignore WordPress.Security.NonceVerification.Recommended
-	exit;
-}
-// Don't accept redirect outside this domain.
-if (!preg_match('{^'.stripslashes($_SERVER['REQUEST_SCHEME']).'://'.stripslashes($_SERVER['SERVER_NAME']).'}', stripslashes($_REQUEST['redirect']))) { //phpcs:ignore WordPress.Security.NonceVerification.Recommended,WordPress.Security.ValidatedSanitizedInput.MissingUnslash,WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
-	exit;
-}
-
+// Place the cookie
+$cookie_domain = preg_replace('{^https?://}', '', site_url());
 setcookie(
 	'selective_preview',
-	$value,
+	$key,
 	0,
 	'/',
-	$_SERVER['SERVER_NAME'], //phpcs:ignore WordPress.Security.ValidatedSanitizedInput.MissingUnslash,WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+	$cookie_domain,
 	true,
 	true
 );
 
-header('location: '.$_REQUEST['redirect'], true, 302); //phpcs:ignore WordPress.Security.NonceVerification.Recommended,WordPress.Security.ValidatedSanitizedInput.MissingUnslash,WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
-
+wp_safe_redirect(site_url()); 

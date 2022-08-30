5cd12bd04da95f8f8246e411f5b2d06bc34bac68
xxsimoxx <simospa@gieffeedizioni.it>
2022-08-30 16:47:55 +0200

    Bump version

--- xsx-under-construction.php
+++ xsx-under-construction.php
@@ -4,7 +4,7 @@
  * -----------------------------------------------------------------------------
  * Plugin Name: Under Construction
  * Description: Redirect not logged in users. Allow testers to see the site sending a magic link.
- * Version: 1.1.0
+ * Version: 1.1.1
  * Requires PHP: 5.6
  * Requires CP: 1.4
  * Author: Simone Fioravanti
